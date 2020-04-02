0b0b616c73caf0f9987f8da4b7954f917059afa2
zkan <kan@prontomarketing.com>
2020-04-02 17:54:24 +0700

    :tada: Create HelloWorld project :bear:

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Kan Ouivirach on 2/4/2563 BE.
+//  Copyright © 2563 ODDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
