7771ea742cd86d1a8132e6e645d181ff213fea9c
zkan <kan@prontomarketing.com>
2020-04-05 09:53:30 +0700

    :sparkles: Add label and image :bear:

---
+++ iOSSharing/iOSSharing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSSharing
+//
+//  Created by Kan Ouivirach on 5/4/2563 BE.
+//  Copyright © 2563 ODDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
