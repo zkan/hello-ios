f9aa2d0de9e0c1cde5e374e1cc5bf7be2764c0af
zkan <zkan.cs@gmail.com>
2020-04-27 10:33:33 +0700

    :tada: Create AutomatedTesting project to try test framework :bear:

---
+++ AutomatedTesting/AutomatedTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutomatedTesting
+//
+//  Created by Kan Ouivirach on 4/27/20.
+//  Copyright © 2020 BearData.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
