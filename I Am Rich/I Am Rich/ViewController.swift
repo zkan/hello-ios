1c082be295cdba149b9d947171b18657160dc41b
zkan <zkan.cs@gmail.com>
2020-05-08 09:12:10 +0700

    :tada: Create I Am Rich project (project from Udemy) :bear:

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kan Ouivirach on 5/8/20.
+//  Copyright © 2020 BearData.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
