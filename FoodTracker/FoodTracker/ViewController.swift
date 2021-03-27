7f3e828854e678be4967b262c251579627012ab0
zkan <zkan.cs@gmail.com>
2021-03-27 22:38:26 +0700

    Create FoodTracker project :bear:

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Kan Ouivirach on 3/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
