b5e7abefca5419f8d5fed156681a25e340601967
zkan <zkan.cs@gmail.com>
2021-07-13 19:57:04 +0700

    Create new project :bear:

---
+++ Trekr/Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Kan Ouivirach on 13/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
