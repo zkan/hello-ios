6577f97a09848c7b21cd033cd1dcb93ecefe830c
zkan <zkan.cs@gmail.com>
2021-07-13 21:03:46 +0700

    Use NavigationView in Preview :bear:

--- Trekr/Trekr/ContentView.swift
+++ Trekr/Trekr/ContentView.swift
@@ -38,6 +38,8 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
