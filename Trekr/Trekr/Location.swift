fd9e50a05aaf0ce3f42f53cc4315555bbe459589
zkan <zkan.cs@gmail.com>
2021-07-13 21:23:46 +0700

    Create struct to hold location data :bear:

---
+++ Trekr/Trekr/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  Trekr
+//
+//  Created by Kan Ouivirach on 13/7/2564 BE.
+//
+
+import Foundation
+
+struct Location {
+    let id: Int
+    let name: String
+    let country: String
+    let description: String
+    let more: String
+    let latitude: Double
+    let longitude: Double
+    let heroPicture: String
+    let advisory: String
+}
