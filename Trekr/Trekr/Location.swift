b521ea9f25443923a52b753dca8949465772a813
zkan <zkan.cs@gmail.com>
2021-07-13 21:47:25 +0700

    Set an example :bear:

--- Trekr/Trekr/Location.swift
+++ Trekr/Trekr/Location.swift
@@ -17,4 +17,7 @@ struct Location {
     let longitude: Double
     let heroPicture: String
     let advisory: String
+    
+    // static means it exists only once and shares between all locations
+    static let example = Location(id: 1, name: "Great Smokey Mountains", country: "United States", description: "A great place to visit.", more: "More text here.", latitude: 35.6532, longitude: -83.5070, heroPicture: "smokies", advisory: "Beware of the bears!")
 }
