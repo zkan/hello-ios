efb708e6ffc1e046d8bb2b94313efece125dcff8
zkan <zkan.cs@gmail.com>
2021-07-13 22:13:34 +0700

    Create class to read JSON file and store all places :bear:

---
+++ Trekr/Trekr/Locations.swift
@@ -0,0 +1,22 @@
+//
+//  Locations.swift
+//  Trekr
+//
+//  Created by Kan Ouivirach on 13/7/2564 BE.
+//
+
+import Foundation
+
+// class is designed to be shared (whereas struct is not)
+// SwiftUI will watch this for changes
+class Locations: ObservableObject {
+    let places: [Location]
+    
+    init() {
+        // ! means it definitely finds the locations.json file
+        let url = Bundle.main.url(forResource: "locations", withExtension: "json")!
+        // If we cannot read the file, the app should crash
+        let data = try! Data(contentsOf: url)
+        places = try! JSONDecoder().decode([Location].self, from: data)
+    }
+}
