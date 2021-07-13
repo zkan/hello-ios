23e00bceac6ce376f94c4cfb77b09a89f31e7f5f
zkan <zkan.cs@gmail.com>
2021-07-13 21:02:05 +0700

    Put things in NavigationView :bear:

--- Trekr/Trekr/ContentView.swift
+++ Trekr/Trekr/ContentView.swift
@@ -32,6 +32,7 @@ struct ContentView: View {
             
             Text("The area is very sparsely populated, with many mountain ranges dominating the region, and includes the highest mountain in the British Isles, Ben Nevis. During the 18th and early 19th centuries the population of the Highlands rose to around 300,000, but from c. 1841 and for the next 160 years, the natural increase in population was exceeded by emigration (mostly to Canada, the United States, Australia and New Zealand, and migration to the industrial cities of Scotland and England.) The area is now one of the most sparsely populated in Europe. At 9.1/km2 (24/sq mi) in 2012, the population density in the Highlands and Islands is less than one seventh of Scotland's as a whole, comparable with that of Bolivia, Chad and Russia.")
         }
+        .navigationTitle("Discover")
     }
 }
 
