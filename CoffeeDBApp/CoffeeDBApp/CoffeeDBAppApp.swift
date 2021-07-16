3f0a9b982d73f86b60bff12f5da51566b6642691
zkan <zkan.cs@gmail.com>
2021-07-16 22:44:47 +0700

    Create a coffee db app :bear:

---
+++ CoffeeDBApp/CoffeeDBApp/CoffeeDBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeDBAppApp.swift
+//  CoffeeDBApp
+//
+//  Created by Kan Ouivirach on 16/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeDBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
