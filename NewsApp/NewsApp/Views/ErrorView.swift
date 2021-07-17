22a4c42669ed9fee63da810c6088858c2808f6d4
zkan <zkan.cs@gmail.com>
2021-07-17 11:21:39 +0700

    Create error view to show error message :bear:

---
+++ NewsApp/NewsApp/Views/ErrorView.swift
@@ -0,0 +1,47 @@
+//
+//  ErrorView.swift
+//  NewsApp
+//
+//  Created by Kan Ouivirach on 17/7/2564 BE.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    
+    let error: Error
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "exclamationmark.icloud.fill")
+                .foregroundColor(.gray)
+                .font(.system(size: 50, weight: .heavy))
+            Text("Ooopss")
+                .foregroundColor(.black)
+                .font(.system(size: 30, weight: .heavy))
+            Text(error.localizedDescription)
+                .foregroundColor(.gray)
+                .font(.system(size: 15))
+                .multilineTextAlignment(.center)
+                .padding(.vertical, 4)
+            Button(action: {
+                
+            }, label: {
+                Text("Retry")
+            })
+            .padding(.vertical, 12)
+            .padding(.horizontal, 30)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .font(.system(size: 15, weight: .heavy))
+            .cornerRadius(10)
+        }
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView(error: APIError.decodingError)
+            .previewLayout(.sizeThatFits)
+    }
+}
