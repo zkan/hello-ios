bd6027337576246fedb58ac4b4b47f3294e430ab
zkan <zkan.cs@gmail.com>
2021-07-17 13:24:27 +0700

    Open URL in Safari :bear:

--- NewsApp/NewsApp/Views/HomeView.swift
+++ NewsApp/NewsApp/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @Environment(\.openURL) var openUrl
     @StateObject var viewModel = NewsViewModelImpl(service: NewsServiceImpl())
     
     var body: some View {
@@ -22,12 +23,22 @@ struct HomeView: View {
                 NavigationView {
                     List(articles) {item in
                         ArticleView(article: item)
+                            .onTapGesture {
+                                load(url: item.url)
+                            }
                     }
                     .navigationTitle(Text("News"))
                 }
             }
         }
         .onAppear(perform: viewModel.getArticles)
+    }
+    
+    func load(url: String?) {
+        guard let link = url,
+              let url = URL(string: link) else { return }
+        
+        openUrl(url)
     }
 }
 
