b557a2b3bdf474a1830abe4f046441d7e44fbb3c
zkan <zkan.cs@gmail.com>
2021-07-17 10:02:01 +0700

    Create a news app :bear:

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Kan Ouivirach on 17/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
