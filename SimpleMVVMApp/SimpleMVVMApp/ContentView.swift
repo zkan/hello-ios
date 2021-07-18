3ead33357b71246fc395a5c5660b4fb2edd6dbec
zkan <zkan.cs@gmail.com>
2021-07-18 12:33:20 +0700

    Create a simple MVVM app :bear:

---
+++ SimpleMVVMApp/SimpleMVVMApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleMVVMApp
+//
+//  Created by Kan Ouivirach on 18/7/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleMVVMApp/SimpleMVVMApp/SimpleMVVMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMVVMAppApp.swift
+//  SimpleMVVMApp
+//
+//  Created by Kan Ouivirach on 18/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMVVMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
