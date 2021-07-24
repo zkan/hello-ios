402de8fc27ed9e0be258ce10582e6e8f0daf5643
zkan <zkan.cs@gmail.com>
2021-07-24 20:21:11 +0700

    Create stacks playground project :bear:

---
+++ Mastering Stacks/Mastering Stacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mastering Stacks
+//
+//  Created by Kan Ouivirach on 24/7/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mastering Stacks/Mastering Stacks/Mastering_StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mastering_StacksApp.swift
+//  Mastering Stacks
+//
+//  Created by Kan Ouivirach on 24/7/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Mastering_StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
