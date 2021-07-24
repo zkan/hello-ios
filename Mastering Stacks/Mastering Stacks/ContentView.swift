0ffce50260b2932176a3f859a9053b1727893ff0
zkan <zkan.cs@gmail.com>
2021-07-24 21:11:55 +0700

    Put view in list on content view :bear:

--- Mastering Stacks/Mastering Stacks/ContentView.swift
+++ Mastering Stacks/Mastering Stacks/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        List {
+            HourlyForecastView()
+                .listRowInsets(EdgeInsets())
+        }
+        .padding()
+        
     }
 }
 
--- Mastering Stacks/Mastering Stacks/Views/HourlyForecastView.swift
+++ Mastering Stacks/Mastering Stacks/Views/HourlyForecastView.swift
@@ -19,12 +19,14 @@ struct HourlyForecastView: View {
             .foregroundColor(.white)
             ForecastListView()
         }
+        .padding()
+        .background(Color.blue.opacity(0.75))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
 struct HourlyForecastView_Previews: PreviewProvider {
     static var previews: some View {
         HourlyForecastView()
-            .background(Color.black)
     }
 }
